4c9f18737af96e16f5e7ede4053cc8c1e004bece
Damien <damien@damienvalade.fr>
2020-09-04 12:42:22 +0200

    Add bootstrap
    Add bootstrap

--- src/Form/CandidateType.php
+++ src/Form/CandidateType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Candidates;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,13 @@ class CandidateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('quality')
-        ;
+            ->add('name', TextType::class, [
+                'label_format' => 'Nom de l\'Argonaute'
+            ])
+            ->add('quality', TextType::class, [
+                'label_format' => 'Qualité',
+                'required' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
